49458152bf0e93379813437b1b654642878cc9ef
Zehra İkizler <zehraaikizler@gmail.com>
2023-02-08 17:23:46 +0300

    project created

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#63605E",
+        secondary: "#948B85",
+        light: "#F5EFEB",
+        dark: "#444346",
+        danger: "#CD5059",
+      },
+      fontFamily: {
+        kalam: ["Kalam", "cursive"],
+        sans: ["Open Sans", "sans-serif"],
+        dancing: ["Dancing Script", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
