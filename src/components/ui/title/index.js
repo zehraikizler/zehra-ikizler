209f8e11f1dd821d0d7c6fbed4b81e2832199d11
Zehra İkizler <zehraaikizler@gmail.com>
2023-02-13 11:49:14 +0300

    about page added

--- src/components/ui/title/index.js
+++ src/components/ui/title/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title({ children }) {
   return (
-    <h3 className="pt-16 mb-10 text-3xl underline font-semibold font-mono text-primary">
+    <h3 className="pt-14 mb-10 text-3xl pb-3 font-semibold font-mono text-primary text-center">
       {children}
     </h3>
   );
--- src/container/about/index.js
+++ src/container/about/index.js
@@ -1,14 +1,47 @@
 import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Title from "@/components/ui/title";
+import Image from "next/image";
 import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="mx-56">
+    <div className="mx-80">
       <Title>About</Title>
-      <Experience />
-      <Education />
+      <div className="mx-16 relative mb-[550px]">
+        <h3 className="font-bold text-9xl text-dark font-mono">
+          Hello, <br /> I'm
+          <br /> Zehra.
+        </h3>
+        <div className="absolute" style={{ right: 0 }}>
+          <Image
+            src="https://media.licdn.com/dms/image/D4D03AQEpP_jIAnIviw/profile-displayphoto-shrink_800_800/0/1670316527183?e=1681948800&v=beta&t=tGVAfswQ6c2YMnnul00B1Xk8oesV1cZ7RZj9QZ-JKLY"
+            alt="profile"
+            className="rounded-2xl"
+            width="450"
+            height="450"
+          />
+        </div>
+      </div>
+
+      <div className="mx-16">
+        <p className="text-xl font-semibold text-dark my-20">
+          Classroom Teaching at Trabzon University as you continue reading in
+          the section I started writing my first code with my interest. I
+          learned HTML, CSS and Bootstrap and did a few projects in the
+          Front-End area. I started learning SCSS and JavaScript to progress
+          further in the software field. After being accepted to the Hi-Kod
+          Atolye organized by the Kodluyoruz team, I started working on
+          React.Js. I continued learning React.Js with my curiosity. I tried to
+          support my development by participating in the trainings held in
+          Trabzon Teknokent. I attended the React Bootcamp organized by
+          Kodluyoruz. I am currently working as Frontend Developer at Kodline
+          company. Detail i like to dwell on it and deal with the details. I
+          never shy away from taking responsibility.
+        </p>
+        <Experience />
+        <Education />
+      </div>
     </div>
   );
 };
