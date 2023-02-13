ca8ea7ee7fa29b33c5645462a0b7fa132a5d1a40
Zehra İkizler <zehraaikizler@gmail.com>
2023-02-13 11:54:23 +0300

    css updated

--- src/components/education/index.js
+++ src/components/education/index.js
@@ -3,12 +3,12 @@ import React from "react";
 const Education = () => {
   return (
     <div className="pb-16">
-      <h2 className="text-lg font-semibold text-dark underline my-3">
+      <h2 className="text-xl font-semibold text-danger underline my-3">
         Education
       </h2>
       <ol className="relative border-l border-secondary">
         <li className="mb-10 ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
+          <div className="absolute w-3 h-3 bg-danger rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             January 2023 - On Going
           </time>
@@ -23,7 +23,7 @@ const Education = () => {
           </p>
         </li>
         <li className="mb-10 ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
+          <div className="absolute w-3 h-3 bg-danger rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             September 2022 - On Going
           </time>
@@ -38,7 +38,7 @@ const Education = () => {
           </p>
         </li>
         <li className="ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
+          <div className="absolute w-3 h-3 bg-danger rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             September 2018 - June 2022
           </time>
--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -3,12 +3,12 @@ import React from "react";
 const Experience = () => {
   return (
     <div className="mb-16">
-      <h2 className="text-lg font-semibold text-dark underline my-3">
+      <h2 className="text-xl font-semibold text-danger underline my-3">
         Experience
       </h2>
       <ol className="relative border-l border-secondary">
         <li className="mb-10 ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
+          <div className="absolute w-3 h-3 bg-danger rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             March 2022 - On Going
           </time>
@@ -23,7 +23,7 @@ const Experience = () => {
           </p>
         </li>
         <li className="mb-10 ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
+          <div className="absolute w-3 h-3 bg-danger rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             July 2021 - October 2021
           </time>
@@ -38,7 +38,7 @@ const Experience = () => {
           </p>
         </li>
         <li className="ml-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
+          <div className="absolute w-3 h-3 bg-danger rounded-full mt-1.5 -left-1.5 border border-secondary"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             June 2021 - July 2021
           </time>
